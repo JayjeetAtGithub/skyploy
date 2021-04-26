5dd3c5964e0d8bd0a994f8c792df4a628359e3ea
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-04-26 17:31:21 +0530

    Purge mons

--- skyploy/commands/do.py
+++ skyploy/commands/do.py
@@ -11,20 +11,16 @@ class Do(Base):
         with open(filepath, 'r') as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
             return data
-    
-    def _purge_cluster(self):
-        cmd = ["ceph-deploy", "purgedata"]
-        cmd.extend(self._config_dict["osd"]["hosts"])
+
+    def _purge_mons(self):
+        cmd = ["ceph-deploy", "mon", "destroy"]
         cmd.extend(self._config_dict["mon"])
-        cmd.extend(self._config_dict["mgr"])
-        cmd.extend(self._config_dict["mds"])
-        print(cmd)
         self._execute(cmd)
 
     def _install_ceph_deploy(self):
         # check if ceph-deploy already cloned
         if not os.path.exists("/tmp/ceph-deploy"):
-            cmd = ["git", "clone", "https://github.com/ceph/ceph-deploy", "/tmp/ceph-deploy"]
+            cmd = ["git", "clone", "https://github.com/JayjeetAtGithub/ceph-deploy", "/tmp/ceph-deploy"]
             self._execute(cmd)
 
         cmd = ["pip3", "install", "--upgrade", "/tmp/ceph-deploy"]
@@ -39,55 +35,42 @@ class Do(Base):
         os.makedirs(self._working_dir, exist_ok=True)
 
     def _create_mon(self):
-        # change into the working dir
-        os.chdir(self._working_dir)
-
+        self._purge_mons()
         # deploy mons
         cmd = ["ceph-deploy", "new"]
         cmd.extend(self._config_dict["mon"])
-        print(cmd)
         self._execute(cmd)
 
         with open("ceph.conf", 'a') as f:
             f.write(f"public_network = {self._config_dict['public_network']}")
 
         cmd = ["ceph-deploy", "--overwrite-conf", "mon", "create-initial"]
-        print(cmd)
         self._execute(cmd)
 
         cmd = ["ceph-deploy", "admin"]
         cmd.extend(self._config_dict["mon"])
-        print(cmd)
         self._execute(cmd)
 
     def _install_daemons(self):
-        # change into the working dir
-        os.chdir(self._working_dir)
-
         cmd = ["ceph-deploy", "install", "--release", self._config_dict["version"]]
         cmd.extend(self._config_dict["osd"]["hosts"])
-        print(cmd)
         self._execute(cmd)
 
     def _create_mgr(self):
-        # change into the working dir
-        os.chdir(self._working_dir)
-
         cmd = ["ceph-deploy", "mgr", "create"]
         cmd.extend(self._config_dict["mgr"])
-        print(cmd)
         self._execute(cmd)
 
     def _create_mds(self):
         pass
 
     def run(self):
+        # change into the working dir
+        os.chdir(self._working_dir)
         config_file_path = str(self.options['<config>'])
         _config_dict = self._read_config(config_file_path)
         self._config_dict = _config_dict
-        print(self._config_dict)
-        self._purge_cluster()
         self._prepare_admin()
         self._install_daemons()
         self._create_mon()
-        self._create_mgr()
+        # self._create_mgr()
